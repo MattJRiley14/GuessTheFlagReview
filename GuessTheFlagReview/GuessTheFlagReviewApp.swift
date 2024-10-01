554618571ac8a44f344369ccd4c3ec58a145c61f
Matt Riley <matthew.riley@barren.kyschools.us>
2024-10-01 08:26:26 -0500

    Initial Commit

---
+++ GuessTheFlagReview/GuessTheFlagReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagReviewApp.swift
+//  GuessTheFlagReview
+//
+//  Created by BCCS 2022 on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
